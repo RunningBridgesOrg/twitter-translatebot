ef35047f679f41d04146bf6e8b0c973af208095e
sujcho <chosujin84@gmail.com>
2017-03-12 19:18:37 -0700

    STREAMING API: FOLLOW A SPECIFIC USER
    - Added a filer 'follow' to follow and get tweets from a specific user
    - Changed variable names to lower case with an underline
    - Read credentials from a separate file

--- oauth_stream.py
+++ oauth_stream.py
@@ -1,35 +1,54 @@
 """
-Author - Anu Vaidya
-Get Streaming tweets for a particular keyword
+Author - Anu Vaidya, Sujin Emily Cho
+date: Mar 12, 2017
+Get Streaming tweets for a specific user
 """
 import tweepy
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 from tweepy import Stream
 
+#Read cfg from a seperated file for security
+def read_cfg_from_file():
+    cfg = []
+    with open("cfg.txt", "r") as f:
+        for line in f:
+            cfg.append(line.strip())
+    return cfg
+
 class MyStreamListener(tweepy.StreamListener):
 	"""A listener handles tweets that are received from the stream.
 	This is a basic listener that just prints received tweets to stdout
-
 	"""
 
 	def on_data(self,data):
-		print(data)
-		return true
+		#This data is in JSON format. needs to be parsed into python string
+		print (data)
+		return True
 
 	def on_error(self,status):
 		print(status)
 
+
+
 def main():
 
-	myStreamListener = MyStreamListener();
+	cfg = read_cfg_from_file()
+	consumer_key = cfg[0]
+	consumer_secret = cfg[1]
+	access_token = cfg[2]
+	access_token_secret = cfg[3]
 
+	my_stream_listener = MyStreamListener();
 	auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 	auth.set_access_token(access_token,access_token_secret)
 	api = tweepy.API(auth)
-	myStream = tweepy.Stream(auth = api.auth,listener=myStreamListener)
-		
-	myStream.filter(track=['PaloAlto'],async=True)
+	my_stream = tweepy.Stream(auth = api.auth,listener=my_stream_listener)
+
+	#list of users to follow. Added value has to be "id" not username.
+	follow_users = ['25073877']
+	
+	my_stream.filter(follow= follow_users, async=True)
 
 if __name__ == "__main__":
-    main()		
+    main()
