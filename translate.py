f64738f7fd0ae1a7393ef33195fee0f0dd231c3a
totoro <totoro@totoros-MacBook-Pro.local>
2017-03-14 00:33:01 -0700

    putting translation response into json format. Extracting just the translated text portion of json response.

--- translate.py
+++ translate.py
@@ -1,11 +1,13 @@
-import requests
+import requests,json
 
 textToTranslate = raw_input("Please enter what you would like to translate:")
 translateTo = raw_input("Language:")
-api_key = 'AI'
+#api_key = 'AI'
 
 payload = {'q':textToTranslate,'target':translateTo,'key':api_key}
 r=requests.get('https://www.googleapis.com/language/translate/v2',params=payload)
 print r.text
 print r.url
-print r
+#response_dict = json.loads(r.text)
+response_dict = r.json()
+print response_dict["data"]["translations"][0]["translatedText"]
