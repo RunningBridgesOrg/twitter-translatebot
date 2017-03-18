53e2c07531f761ca8f8a8240c3612d2230a533de
sujcho <chosujin84@gmail.com>
2017-03-17 19:11:46 -0700

    GET TWEET_TEXT & TWEET_LANG
    -Retrieving tweet data (JSON) into python
    -Getting text and language

--- oauth_stream.py
+++ oauth_stream.py
@@ -3,12 +3,27 @@ Author - Anu Vaidya, Sujin Emily Cho
 date: Mar 12, 2017
 Get Streaming tweets for a specific user
 """
+
 import tweepy
+
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 from tweepy import Stream
+import json
 
-#Read cfg from a seperated file for security
+
+class MyStreamListener(tweepy.StreamListener):
+    def on_data(self, data):
+        #print (data)
+        all_data = json.loads(data)
+        tweet_text = all_data['text']
+        tweet_lang = all_data['lang']
+        print(tweet_text + tweet_lang)
+        return True
+
+    def on_error(self,status):
+        print(status)
+
 def read_cfg_from_file():
     cfg = []
     with open("cfg.txt", "r") as f:
@@ -16,39 +31,23 @@ def read_cfg_from_file():
             cfg.append(line.strip())
     return cfg
 
-class MyStreamListener(tweepy.StreamListener):
-	"""A listener handles tweets that are received from the stream.
-	This is a basic listener that just prints received tweets to stdout
-	"""
-
-	def on_data(self,data):
-		#This data is in JSON format. needs to be parsed into python string
-		print (data)
-		return True
-
-	def on_error(self,status):
-		print(status)
-
-
-
 def main():
+    cfg = read_cfg_from_file()
+    consumer_key = cfg[0]
+    consumer_secret = cfg[1]
+    access_token = cfg[2]
+    access_token_secret = cfg[3]
 
-	cfg = read_cfg_from_file()
-	consumer_key = cfg[0]
-	consumer_secret = cfg[1]
-	access_token = cfg[2]
-	access_token_secret = cfg[3]
+    my_stream_listener = MyStreamListener();
+    auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+    auth.set_access_token(access_token,access_token_secret)
+    api = tweepy.API(auth)
+    my_stream = tweepy.Stream(auth = api.auth,listener=my_stream_listener)
 
-	my_stream_listener = MyStreamListener();
-	auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-	auth.set_access_token(access_token,access_token_secret)
-	api = tweepy.API(auth)
-	my_stream = tweepy.Stream(auth = api.auth,listener=my_stream_listener)
+    #list of users to follow. Added value has to be "id" not username.
+    follow_users = ['25073877','838107760721985536']
 
-	#list of users to follow. Added value has to be "id" not username.
-	follow_users = ['25073877']
-	
-	my_stream.filter(follow= follow_users, async=True)
+    my_stream.filter(follow= follow_users, async=True)
 
 if __name__ == "__main__":
     main()
