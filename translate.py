40e8b60fd4699b3f724de7a73ce3a974522a83ee
totoro <totoro@totoros-MacBook-Pro.local>
2017-03-22 19:49:44 -0700

    created a main function

--- translate.py
+++ translate.py
@@ -1,13 +1,15 @@
 import requests,json
 
-textToTranslate = raw_input("Please enter what you would like to translate:")
-translateTo = raw_input("Language:")
-#api_key = 'AI'
 
-payload = {'q':textToTranslate,'target':translateTo,'key':api_key}
-resp=requests.get('https://www.googleapis.com/language/translate/v2',params=payload)
-#print r.text
-#print r.url
-response_dict = resp.json()
-print response_dict["data"]["translations"][0]["translatedText"]
+def main():
+    textToTranslate = raw_input("Please enter what you would like to translate:")
+    translateTo = raw_input("Language:")
+    #api_key = 'AI'
+    
+    payload = {'q':textToTranslate,'target':translateTo,'key':api_key}
+    resp=requests.get('https://www.googleapis.com/language/translate/v2',params=payload)
+    response_dict = resp.json()
+    print response_dict["data"]["translations"][0]["translatedText"]
 
+if __name__ == "__main__":
+    main()
