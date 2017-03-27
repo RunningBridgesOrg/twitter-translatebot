ccebb1518f94f0229ec1789aea6758ab10ca2b77
anuvaidya <anusubra@yahoo.com>
2017-03-26 19:32:17 -0700

    modified oauth to class, added mock test

---
+++ test_oauth_stream.py
@@ -0,0 +1,22 @@
+from unittest.mock import MagicMock, patch
+import json
+import oauth_stream
+import unittest
+
+@patch('tweepy.Stream')
+@patch('tweepy.OAuthHandler')
+def test_json_func(m_tweepy_OAuthHandler,m_tweepy_stream):
+	print(locals())
+	m_tweepy_stream.return_value = MagicMock()
+	m_tweepy_stream.__dir__ = ({"text": "cab"})
+	oauth_stream.TwitterListener().main()
+	#assert oauth_stream.TwitterListener().main() == 2345 # use a valid id
+
+	m_tweepy_OAuthHandler.return_value = MagicMock()
+	m_tweepy_OAuthHandler.assert_called_with('','')
+
+
+    
+
+
+test_json_func()
