21d2774a02a6c47abc01b9f4f5e232c089b2d06d
sujcho <chosujin84@gmail.com>
2017-05-07 12:33:55 -0700

    same code with some comments about dependency injection.

--- twitterListener.py
+++ twitterListener.py
@@ -13,17 +13,19 @@ import json
 #this_id = ['838107760721985536','25073877']
 class MyStreamListener(tweepy.StreamListener):
     def __init__(self, user_list):
-
         print("init")
         self.__this_id = user_list
-        print("this.id"  + self.__this_id[0])
 
+        #    print("this.id"  + self.__this_id[0])
+
+        #This code is for testing
         # real_tweet = {"text": "Four more years. http:\/\/t.co\/bAJE6Vom",
         # "user": {"id": 266031293949698048,"id_str": "266031293949698048"}
         # }
         #
         # self.on_data(json.dumps(real_tweet))
 
+        #This code is for testing
     #def __call__(self):
         # print ("calling")
         # real_tweet = {"text": "Four more years. http:\/\/t.co\/bAJE6Vom",
@@ -37,7 +39,6 @@ class MyStreamListener(tweepy.StreamListener):
         tweet_obj={}
         author = all_data['user']['id_str']
         #print (author)
-        #print (this_id)
 
         if(author in self.__this_id) :
             print("same?")
@@ -58,12 +59,15 @@ class TwitterListener:
         return user_list;
 
     def __init__(self):
+    #def __init__(self, service):
         #private attribute
         #self.__user_list = []
         self.__cfg = self.__read_cfg_from_DB()
         self.__user_list = self.__get_userlist()
         #MyStreamListener instance is created inside TwitterListener
         self.myStreamListener =  MyStreamListener(self.__user_list);
+        #? If we create the service outside, how can we see the user list?, user list is a private variable.
+        #self.myStreamListener = service
 
     def __read_cfg_from_DB(self):
         cfg = {}
@@ -94,11 +98,17 @@ class TwitterListener:
         #self.__users_list = self.__get_userlist()
 
     #    my_stream_listener = MyStreamListener();
+    #Do we also need depenedcy injection for stream instance?
         my_stream = tweepy.Stream(auth = api.auth,listener=self.myStreamListener)
         my_stream.filter(follow= self.__user_list, async=True)
 
 
 if __name__ == "__main__":
+
+    #This code is for depenedcy injection
+    #myStreamListenerService = MyStreamListener()
+    #MyTwitterListener = TwitterListener(myStreamListenerService)
+
     MyTwitterListener = TwitterListener()
     MyTwitterListener.main()
     #TwitterListener().main()
