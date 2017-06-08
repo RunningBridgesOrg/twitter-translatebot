0dcb02cce1d405558310d739cb8e3a7c557f9ead
Michelle <michelleyho@users.noreply.github.com>
2017-06-08 03:59:02 -0700

    finished code.

--- translate2.py
+++ translate2.py
@@ -1,16 +1,30 @@
 import collections
 import requests,json
 
-class dbReader():
+global fName 
+fName = "apiKey.txt"
+
+class DBReader():
     def __init__(self):
         self.translateApiKey = ""
     def getTranslateKey(self):
         return self.translateApiKey
-    def setTranslateKey(self,keyValue):
-        self.translateApiKey = keyValue
+    def setTranslateKey(self):
+        try:
+            with open(fName,"r") as f:
+                self.translateApiKey = ""
+                for line in f:
+                    self.translateApiKey = line.strip()
+                    print self.translateApiKey
+                
+                #Error condition checking for existing key.
+                if not self.translateApiKey:
+                    print "No api key is found"
+        except IOError:
+            print "Could not read file: '%s'"%(fName)
 
 class TranslationObj():
-    def __init_(self,text,language):
+    def __init__(self,text,language):
         self.text = text
         self.targetLang = language
     def getText(self):
@@ -19,9 +33,9 @@ class TranslationObj():
         return self.targetLang
 
 
-class translate():
+class Translate():
     def __init__(self,dbReader,translateObj):
-        self.apiKey = dbReader.getKey()
+        self.apiKey = dbReader.getTranslateKey()
         self.origText = translateObj.getText()
         self.targetLang = translateObj.getTargetLang()
         self.translatedText = ""
@@ -39,3 +53,25 @@ class translate():
                 print "Error Code:%s Message:%s Reason:%s"%(response_dict["error"]["code"],response_dict["error"]["message"],response_dict["error"]["errors"][0]["reason"])
                 self.responseCode = response_dict["error"]["code"]
 
+
+def main():
+    db = DBReader()
+    db.setTranslateKey()
+    tweet1 = TranslationObj("Let them eat cake","fr")
+    tweet2 = TranslationObj("Doing demonstration is hard","ko")
+    tweet3 = TranslationObj("I hope this works","zh-TW")
+    tweet4 = TranslationObj("We are moving on Friday","es")
+
+    translate1 = Translate(db,tweet1)
+    translate2 = Translate(db,tweet2)
+    translate3 = Translate(db,tweet3)
+    translate4 = Translate(db,tweet4)
+
+    translate1.getTranslation()
+    translate2.getTranslation()
+    translate3.getTranslation()
+    translate4.getTranslation()
+
+    
+if __name__ == "__main__":
+    main()
